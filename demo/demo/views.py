a6e8df842cd05b31451dd6bdffb6e937af173ef0
Eric Lo <lxneng@gmail.com>
2012-02-24 15:21:07 +0800

    Revert "fix renren.com default scope"
    This reverts commit c7765d4de415ed5c3b3346c5470e2d265d5f893d.

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -1,8 +1,9 @@
 from pyramid.view import view_config
 
+from velruse.api import login_url
 
 @view_config(
-    context='velruse.AuthenticationComplete',
+    context='velruse.api.AuthenticationComplete',
     renderer='json',
 )
 def auth_complete_view(context, request):
@@ -12,8 +13,17 @@ def auth_complete_view(context, request):
     }
 
 @view_config(
-    context='velruse.AuthenticationDenied',
+    context='velruse.exceptions.AuthenticationDenied',
     renderer='json',
 )
 def auth_denied_view(context, request):
-    return context.reason
+    return context.args
+
+@view_config(
+    name='login',
+    renderer='demo:templates/login.mako',
+)
+def login_view(request):
+    return {
+        'login_url': lambda name: login_url(request, name),
+    }
